c234499a4a850c1b2ef84a6a0bcae70dda62b82e
Stjepan Mrgnaić <stjepan.mrganic@gmail.com>
2021-06-12 20:02:47 +0200

    Finished refactor

--- app/src/main/java/com/example/geoguesser/fragments/MapFragment.kt
+++ app/src/main/java/com/example/geoguesser/fragments/MapFragment.kt
@@ -20,37 +20,45 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class MapFragment : SupportMapFragment(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
-    private lateinit var mMap: GoogleMap
     private var resetGame = false
+    private var marker: Marker? = null
+    private lateinit var map: GoogleMap
+
+    private val preferences: Preferences by inject()
     private val soundPoolPlayer: AudioPlayer by inject()
     private val viewModel by sharedViewModel<LocationViewModel>()
-    private var marker: Marker? = null
-    private val preferences: Preferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val inflater = TransitionInflater.from(activity)
 
+        // Set transitions
+        val inflater = TransitionInflater.from(activity)
         enterTransition = inflater.inflateTransition(R.transition.fade)
         exitTransition = inflater.inflateTransition(R.transition.slide_right)
 
+        // Start async task to get map, respond in callback on completion
         getMapAsync(this)
 
+        // Observe game status and map data for changes
         viewModel.gameLiveData.observe(this, { resetGame = it.getResetGame() })
         viewModel.mapLiveData.observe(this, { marker = it.getMarker() })
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        mMap.setOnMapLongClickListener(this)
+        map = googleMap
+        map.setOnMapLongClickListener(this)
     }
 
     override fun onMapLongClick(clickPosition: LatLng) {
+
+        // Disable click if game is waiting for reset
         if (resetGame) return
 
+        // Play pop sound on pin drop
         soundPoolPlayer.playSound(R.raw.marker)
 
-        mMap.run {
+        // Clear all other pins. Add a new one on click position and notify observers of state change
+        map.run {
             clear()
 
             marker = addMarker(
@@ -60,7 +68,7 @@ class MapFragment : SupportMapFragment(), OnMapReadyCallback, GoogleMap.OnMapLon
 
             viewModel.setMarker(MapElementsData(marker!!))
 
-            animateCamera(CameraUpdateFactory.newLatLngZoom(clickPosition, 5f))
+            animateCamera(CameraUpdateFactory.newLatLngZoom(clickPosition, 7.5f))
         }
     }
 
@@ -76,42 +84,52 @@ class MapFragment : SupportMapFragment(), OnMapReadyCallback, GoogleMap.OnMapLon
 
         val formattedResult = String.format("%.2f", results[0] / 1000)
 
+        // Set high score if necessary
         if (preferences.getHighScore() > results[0]) preferences.setHighScore(results[0])
 
         return formattedResult
     }
 
     fun drawPolyLine(selectedPosition: LatLng, actualPosition: LatLng) {
-        val poly = mMap.addPolyline(
+
+        // Add polyline start and end points
+        val poly = map.addPolyline(
             PolylineOptions().clickable(false).add(selectedPosition).add(actualPosition)
         )
+
+        // Style the polyline
         poly.width = 12f
         poly.color = Color.DKGRAY
         poly.pattern = listOf(Dot(), Gap(20f))
 
-        mMap.addMarker(
+        // Add a marker at the end of the polyline.
+        map.addMarker(
             MarkerOptions().position(actualPosition).title("Street view Location")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
         )
 
         val result = calculateResult(selectedPosition, actualPosition)
 
+        // Calculate info windows position
         val xValue = (selectedPosition.latitude + actualPosition.latitude) / 2
         val yValue = (selectedPosition.longitude + actualPosition.longitude) / 2
         val infoWindowPosition = LatLng(xValue, yValue)
 
+        // Add the info window using an extension function
         poly.addInfoWindow(
-            mMap,
+            map,
             "Distance",
             "You guessed $result km from the correct location.",
             infoWindowPosition
         )
 
+        // Adjust zoom so that the entire polyline is on screen
         val latLngBoundsBuilder = LatLngBounds.builder()
         latLngBoundsBuilder.include(selectedPosition)
         latLngBoundsBuilder.include(actualPosition)
         val bounds = latLngBoundsBuilder.build()
 
-        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200))
+        // Animate camera to new position
+        map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200))
     }
 }
--- app/src/main/java/com/example/geoguesser/fragments/StreetViewFragment.kt
+++ app/src/main/java/com/example/geoguesser/fragments/StreetViewFragment.kt
@@ -1,7 +1,5 @@
 package com.example.geoguesser.fragments
 
-import android.graphics.Color
-import android.location.Location
 import android.os.Bundle
 import android.transition.TransitionInflater
 import com.example.geoguesser.R
@@ -9,16 +7,15 @@ import com.example.geoguesser.mvvm.LocationData
 import com.example.geoguesser.mvvm.LocationViewModel
 import com.example.geoguesser.network.Networking
 import com.example.geoguesser.network.Parser
-import com.example.geoguesser.utils.Preferences
-import com.example.geoguesser.utils.addInfoWindow
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback
+import com.google.android.gms.maps.StreetViewPanorama
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment
+import com.google.android.gms.maps.model.LatLng
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.IOException
 
 class StreetViewFragment : SupportStreetViewPanoramaFragment(), OnStreetViewPanoramaReadyCallback {
@@ -26,48 +23,61 @@ class StreetViewFragment : SupportStreetViewPanoramaFragment(), OnStreetViewPano
     private val networking: Networking by inject()
     private val parser by inject<Parser<String, LatLng>>()
 
+    // Share view Model with maps fragment and maps activity
     private val viewModel by sharedViewModel<LocationViewModel>()
     private lateinit var position: LatLng
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Set transitions
         val inflater = TransitionInflater.from(activity)
         this.enterTransition = inflater.inflateTransition(R.transition.fade)
         this.exitTransition = inflater.inflateTransition(R.transition.slide_left)
 
+        // Start async task to get panorama, respond in callback on completion
         getStreetViewPanoramaAsync(this)
     }
 
     override fun onStreetViewPanoramaReady(panorama: StreetViewPanorama) {
+        // Disable street names, that would not be a fun game
         panorama.isStreetNamesEnabled = false
+
+        // Set the location of the newly created panorama
         setNewLocation(panorama)
     }
 
     private fun setNewLocation(panorama: StreetViewPanorama) {
 
+        // Create callback for async network request
         val callback = object : Callback {
 
             override fun onFailure(call: Call, e: IOException) = e.printStackTrace()
 
             override fun onResponse(call: Call, response: Response) {
-                var string: String?
-                response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-                    string = response.body?.string()
+                if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-                    string?.let {
-                        activity?.runOnUiThread {
-                            position = parser.parse(it)
-                            panorama.setPosition(position, 2500)
-                            viewModel.setData(LocationData(position, panorama))
-                        }
+                val string: String? = response.body?.string()
+
+                // If string is not null we have valid coordinates.
+                // And load the random location into panorama within Street View Fragment
+                string?.let {
+
+                    position = parser.parse(it)
+
+                    // Panorama and observers need to be handled on the UI thread
+                    activity?.runOnUiThread {
+                        panorama.setPosition(position, 2500)
+                        viewModel.setData(LocationData(position, panorama))
                     }
                 }
+
             }
+
         }
 
+        // Get new random coordinates from API
         networking.execute(callback, "https://api.3geonames.org/?randomland=HR&json=1")
 
     }
